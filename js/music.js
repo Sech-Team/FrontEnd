b3affdf2394cfa8f94ce9ebec93cdc82b2ad0670
pnthuc <24120228@student.hcmus.edu.vn>
2024-12-07 00:53:10 +0700

    COMMIT CUOI CUNG

--- js/music.js
+++ js/music.js
@@ -1,4 +1,4 @@
-var a = ["ms/Royal & the Serpent - “Wasteland” (from Arcane Season 2) [Official Music Video].mp3", "ms/Stray Kids, Young Miko, Tom Morello - _Come Play_ (from Arcane Season 2) [Official Visualizer].mp3", "ms/Stromae, Pomme - “Ma Meilleure Ennemie” (from Arcane Season 2) [Official Visualizer].mp3"];
+var a = ["ms/J.Fla - Feliz Navidad.mp3"];
 var counter = 0;
 const audio = document.getElementById('theme-music');
 window.onload = function() {
