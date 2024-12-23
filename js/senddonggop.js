001620c63ed31d83788ee5466348d6fbff11f8cb
pnthuc <24120228@student.hcmus.edu.vn>
2024-12-24 01:21:25 +0700

    fix link

--- js/senddonggop.js
+++ js/senddonggop.js
@@ -2,7 +2,7 @@ function senddonggop(number) {
     if (number === null || number === '' || isNaN(number)) {
         return alert("Vui lòng nhập số hợp lệ");
     }
-    fetch("http://127.0.0.1:5000/get", {
+    fetch("https://backend-fhuz.onrender.com/get", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
